09ea9d5af39e067fc676fb6325b757de2e1925bc
risingisland <risingisland@yahoo.com>
2024-01-08 11:11:52 +0000

    v1.1

---
+++ hostingSpace/lang/es_ES.php
@@ -0,0 +1,13 @@
+<?php 
+
+	$i18n = array(
+		'HOSTINGNAME'	=> 'Detector de límite de espacio de alojamiento',
+        'WEBSITEUSES'	=> 'El sitio web utiliza:',
+        'INFORMADMIN'	=> 'Informar al admin cuando el sitio web supere el límite de (MB):',
+        'SPACEOVER'		=> 'Correo para recibir notificación cuando se acabe el espacio',
+        'PLACEHOLDERMB'	=> 'Ingrese cuantos megas sin las letras MB',
+        'PAYPAL'		=> 'Si quieres apoyar mi trabajo vía paypal :) ¡Gracias!',
+        'SAVESETTINGS'	=> 'Guardar ajustes',
+ 	);
+
+;?>
